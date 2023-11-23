163a7dca2442816c234d40d304ee128b74937cd8
shubhamyemurle <125962960+shubhamyemurle@users.noreply.github.com>
2023-11-23 16:57:11 +0530

    Add New Folders

---
+++ JS/resp.js
@@ -0,0 +1,15 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-List')
+rightNav = document.querySelector('.rightNav')
+
+
+
+burger. addEventListener('Click', ()=>{
+
+    rightNav.classList. toggle('v-class-resp');
+    navList.classList. toggle('v-class-resp');
+    navbar.classList. toggle('h-nav-resp');
+
+
+})
